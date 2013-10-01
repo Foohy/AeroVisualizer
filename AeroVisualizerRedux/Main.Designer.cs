b8f185ed89330c6f76c45dfd7ab75cb66c998b02
Foohy <swkauker@yahoo.com>
2013-10-01 08:05:54 -0700

    Moved around some controls

--- AeroVisualizerRedux/Main.Designer.cs
+++ AeroVisualizerRedux/Main.Designer.cs
@@ -35,6 +35,7 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.comboDeviceSelect = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numUpdateInterval)).BeginInit();
             this.SuspendLayout();
@@ -44,19 +45,20 @@
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.btnAdvanced);
+            this.groupBox1.Controls.Add(this.checkBox1);
             this.groupBox1.Controls.Add(this.numUpdateInterval);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Location = new System.Drawing.Point(12, 26);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(276, 75);
+            this.groupBox1.Size = new System.Drawing.Size(276, 80);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Settings";
             // 
             // btnAdvanced
             // 
-            this.btnAdvanced.Location = new System.Drawing.Point(195, 45);
+            this.btnAdvanced.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdvanced.Location = new System.Drawing.Point(213, 112);
             this.btnAdvanced.Name = "btnAdvanced";
             this.btnAdvanced.Size = new System.Drawing.Size(75, 23);
             this.btnAdvanced.TabIndex = 12;
@@ -118,11 +120,22 @@
             this.comboDeviceSelect.TabIndex = 2;
             this.comboDeviceSelect.SelectedIndexChanged += new System.EventHandler(this.comboDeviceSelect_SelectedIndexChanged);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(9, 46);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Smooth FFT Data";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 113);
+            this.ClientSize = new System.Drawing.Size(300, 147);
+            this.Controls.Add(this.btnAdvanced);
             this.Controls.Add(this.comboDeviceSelect);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
@@ -147,6 +160,7 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.ComboBox comboDeviceSelect;
         private System.Windows.Forms.Button btnAdvanced;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
